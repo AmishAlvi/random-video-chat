9d35e9d106307c08cd532e76fb38e5ff4f5fb63a
AmishAlvi <amish.alvi@gmail.com>
2022-05-30 10:11:29 +0500

    re adding cors policies

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const app = require("express")();
 const server = require("http").createServer(app);
-//const cors = require("cors");
-//app.use(cors());
+const cors = require("cors");
+
 
 const io = require("socket.io")(server, {
-	// cors: {
-	// 	origin: '*',
-	//   }
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
 });
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
