075b9afe0a48e39be67e1058542135601aca29e0
AmishAlvi <amish.alvi@gmail.com>
2022-05-30 09:53:17 +0500

    removing all cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const server = require("http").createServer(app);
-const cors = require("cors");
+//const cors = require("cors");
 //app.use(cors());
 
 const io = require("socket.io")(server, {
