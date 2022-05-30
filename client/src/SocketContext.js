e77cc570d3256bca5626761a52c964975d158758
AmishAlvi <amish.alvi@gmail.com>
2022-05-30 12:08:32 +0500

    sending requests with credentials to socket io

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -5,7 +5,9 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 //const socket = io('http://localhost:5000');
-const socket = io('https://random-video-chat-server.vercel.app/');
+const socket = io('https://random-video-chat-server.vercel.app/', {
+  withCredentials: true
+});
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
