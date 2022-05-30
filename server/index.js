5fe9b2a27c092706b4a75dff671538ec39e72703
AmishAlvi <amish.alvi@gmail.com>
2022-05-30 11:58:53 +0500

    checking if localhost works

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: '*',
+		origin: 'https://localhost:3000',
 		methods: [ "GET", "POST" ]
 	}
 });
