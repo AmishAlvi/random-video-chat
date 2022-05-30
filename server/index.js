dda50987273324ac72f0e62caba1a747d0d68a97
AmishAlvi <amish.alvi@gmail.com>
2022-05-30 09:34:40 +0500

    making cors policy work

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
+app.use(cors());
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -8,10 +9,6 @@ const io = require("socket.io")(server, {
 		methods: [ "GET", "POST" ]
 	}
 });
-
-
-
-app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
